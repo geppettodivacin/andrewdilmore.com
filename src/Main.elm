7136e24efcb647d39d2488d28d35becac3a9ac08
geppettodivacin <ericdilmore@gmail.com>
2018-12-16 18:33:15 -0600

    Make the functional image viewer way prettier.

--- src/Main.elm
+++ src/Main.elm
@@ -2,6 +2,7 @@ module Main exposing (main)
 
 import Browser
 import Browser.Dom as Dom
+import Browser.Events
 import Browser.Navigation as Navigation
 import Dict exposing (Dict)
 import Element exposing (..)
@@ -18,14 +19,73 @@ import Json.Encode as Encode
 import List.Extra as List
 import SelectList exposing (SelectList)
 import Task
+import Url exposing (Url)
+import Url.Builder as Builder
+import Url.Parser as Url exposing (..)
+import Url.Parser.Query as Query
 
 
 main =
-    Browser.sandbox
+    Browser.application
         { init = init
         , view = view
         , update = update
+        , subscriptions = subscriptions
+        , onUrlRequest = ClickedLink
+        , onUrlChange = ChangedUrl
         }
+
+
+
+-- ROUTES
+
+
+route : Model -> Parser (Page -> a) a
+route model =
+    oneOf
+        [ Url.map (toFullSizePage model) (s "full" <?> Query.string "image")
+        , Url.map Thumbnails top
+        ]
+
+
+toPage : Model -> Url -> Page
+toPage model url =
+    Maybe.withDefault NotFound (Url.parse (route model) url)
+
+
+
+-- TODO: Make the directory listing remote data, and use a proper loading page
+-- for when we don't have a listing yet.
+
+
+toFullSizePage : Model -> Maybe String -> Page
+toFullSizePage model imageSrc =
+    case Maybe.andThen (makeFullSize model.dirListing model.filterPath) imageSrc of
+        Nothing ->
+            NotFound
+
+        Just data ->
+            FullSize data
+
+
+homeUrl : String
+homeUrl =
+    thumbnailsUrl
+
+
+thumbnailsUrl : String
+thumbnailsUrl =
+    Builder.absolute [] []
+
+
+fullSizeUrl : String -> String
+fullSizeUrl imageSrc =
+    Builder.absolute [ "full" ] [ Builder.string "image" imageSrc ]
+
+
+assetUrl : String -> String
+assetUrl src =
+    Builder.absolute [ "assets", src ] []
 
 
 
@@ -33,15 +93,24 @@ main =
 
 
 type alias Model =
-    { page : Page
+    { key : Navigation.Key
+    , viewport : Viewport
+    , page : Page
     , dirListing : DirListing
     , filterPath : FilterPath
+    }
+
+
+type alias Viewport =
+    { width : Int
+    , height : Int
     }
 
 
 type Page
     = Thumbnails
     | FullSize FullSizeData
+    | NotFound
 
 
 type alias FullSizeData =
@@ -62,12 +131,20 @@ type FilterPath
     = FilterPath String (List String)
 
 
-init : Model
-init =
-    { page = Thumbnails
-    , dirListing = defaultListing
-    , filterPath = FilterPath "images" []
-    }
+init : {} -> Url -> Navigation.Key -> ( Model, Cmd Msg )
+init flags url key =
+    let
+        initialModel =
+            { key = key
+            , viewport = { width = 1000, height = 900 }
+            , page = Thumbnails
+            , dirListing = defaultListing
+            , filterPath = FilterPath "images" []
+            }
+    in
+    ( { initialModel | page = toPage initialModel url }
+    , Task.perform (\size -> WindowResize (round size.viewport.width) (round size.viewport.height)) Dom.getViewport
+    )
 
 
 defaultListing : DirListing
@@ -114,35 +191,49 @@ currentFilter filterPath =
                 |> Maybe.withDefault ""
 
 
-makeFullSize : DirListing -> String -> String -> FullSizeData
-makeFullSize listing dir current =
+makeFullSize : DirListing -> FilterPath -> String -> Maybe FullSizeData
+makeFullSize listing filter current =
     let
         doThing ( start, end ) =
             case end of
                 [] ->
-                    SelectList.fromList start
-                        |> Maybe.withDefault (SelectList.singleton "")
+                    Nothing
 
-                elements ->
-                    List.tail elements
-                        |> Maybe.withDefault []
-                        |> (\rest -> SelectList.fromLists start current rest)
+                head :: tail ->
+                    Just (SelectList.fromLists start head tail)
     in
     listing
-        |> filesInDir dir
+        |> filesInDir (currentFilter filter)
         |> List.splitWhen (\file -> file == current)
-        |> Maybe.withDefault ( [], [] )
-        |> doThing
+        |> Maybe.andThen doThing
 
 
-nextImage : FullSizeData -> FullSizeData
-nextImage =
-    SelectList.attempt (SelectList.selectBy 1)
+nextImage : Navigation.Key -> FullSizeData -> ( FullSizeData, Cmd Msg )
+nextImage key data =
+    let
+        newData =
+            SelectList.attempt (SelectList.selectBy 1) data
+
+        newUrl =
+            newData
+                |> SelectList.selected
+                |> fullSizeUrl
+    in
+    ( newData, Navigation.pushUrl key newUrl )
 
 
-prevImage : FullSizeData -> FullSizeData
-prevImage =
-    SelectList.attempt (SelectList.selectBy -1)
+prevImage : Navigation.Key -> FullSizeData -> ( FullSizeData, Cmd Msg )
+prevImage key data =
+    let
+        newData =
+            SelectList.attempt (SelectList.selectBy -1) data
+
+        newUrl =
+            newData
+                |> SelectList.selected
+                |> fullSizeUrl
+    in
+    ( newData, Navigation.pushUrl key newUrl )
 
 
 
@@ -151,58 +242,126 @@ prevImage =
 
 type Msg
     = NoOp
+    | WindowResize Int Int
+    | ClickedLink Browser.UrlRequest
+    | ChangedUrl Url
     | OpenImage String
     | NextImage
     | PrevImage
     | CloseImage
 
 
-update : Msg -> Model -> Model
+update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         NoOp ->
-            model
+            ( model, Cmd.none )
+
+        WindowResize width height ->
+            ( { model | viewport = { width = width, height = height } }, Cmd.none )
+
+        ClickedLink request ->
+            case request of
+                Browser.Internal url ->
+                    ( model, Navigation.pushUrl model.key (Url.toString url) )
+
+                Browser.External href ->
+                    ( model, Navigation.load href )
+
+        ChangedUrl url ->
+            let
+                page =
+                    toPage model url
+            in
+            ( { model | page = page }, Cmd.none )
 
         OpenImage img ->
             let
                 newPage =
-                    FullSize (makeFullSize model.dirListing (currentFilter model.filterPath) img)
+                    toFullSizePage model (Just img)
             in
-            { model | page = newPage }
+            ( { model | page = newPage }, Cmd.none )
 
         NextImage ->
             case model.page of
                 FullSize data ->
-                    { model | page = FullSize (nextImage data) }
+                    let
+                        ( newData, cmd ) =
+                            nextImage model.key data
+
+                        newPage =
+                            FullSize newData
+                    in
+                    ( { model | page = newPage }, cmd )
 
                 _ ->
-                    model
+                    ( model, Cmd.none )
 
         PrevImage ->
             case model.page of
                 FullSize data ->
-                    { model | page = FullSize (prevImage data) }
+                    let
+                        ( newData, cmd ) =
+                            prevImage model.key data
+
+                        newPage =
+                            FullSize newData
+                    in
+                    ( { model | page = newPage }, cmd )
 
                 _ ->
-                    model
+                    ( model, Cmd.none )
 
         CloseImage ->
-            { model | page = Thumbnails }
+            ( { model | page = Thumbnails }, Cmd.none )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Browser.Events.onResize WindowResize
 
 
 
 -- VIEW
 
 
-view : Model -> Html Msg
+view : Model -> Browser.Document Msg
 view model =
-    column
-        [ width fill, height fill ]
-        [ siteHeader
-        , pageContent model
-        , siteFooter
+    { title = "Andrew Dilmore"
+    , body =
+        [ column
+            [ width fill, height fill, spacing 10 ]
+            [ siteHeader
+            , pageContent model
+            , siteFooter
+            ]
+            |> el [ width fill, height fill, withBackground ]
+            |> Element.layout []
         ]
-        |> Element.layout []
+    }
+
+
+withBackground : Attribute Msg
+withBackground =
+    let
+        leftImage =
+            image [ alignLeft, moveLeft 260 ]
+                { description = "", src = assetUrl "backgroundSide.png" }
+
+        rightImage =
+            image [ alignRight, moveRight 260 ]
+                { description = "", src = assetUrl "backgroundSide.png" }
+    in
+    row
+        [ height (fill |> maximum 600), width fill, clip ]
+        [ leftImage
+        , rightImage
+        ]
+        |> behindContent
 
 
 pageContent : Model -> Element Msg
@@ -212,77 +371,85 @@ pageContent model =
             thumbnailListElement model
 
         FullSize fullSizeData ->
-            fullSizeElement fullSizeData
+            fullSizeElement model.viewport fullSizeData
+
+        NotFound ->
+            el [ centerX ] (text "Not found, buddy")
 
 
 thumbnailListElement : Model -> Element Msg
 thumbnailListElement model =
     model.dirListing
         |> filesInDir (currentFilter model.filterPath)
-        |> chunksOf 3
-        |> thumbnailColumn
+        |> chunksOf rowLength
+        |> thumbnailColumn model.viewport
 
 
-thumbnailColumn : List (List String) -> Element Msg
-thumbnailColumn rows =
+thumbnailColumn : Viewport -> List (List String) -> Element Msg
+thumbnailColumn viewport rows =
     rows
-        |> List.map thumbnailRow
-        |> column [ spacing 20, Background.color debugColors.column ]
+        |> List.map (thumbnailRow viewport)
+        |> column [ centerX, spacing 20 ]
 
 
-thumbnailRow : List String -> Element Msg
-thumbnailRow srcs =
+thumbnailRow : Viewport -> List String -> Element Msg
+thumbnailRow viewport srcs =
     srcs
-        |> List.map thumbnailElement
-        |> row [ spacing 20, Background.color debugColors.row ]
+        |> List.map (thumbnailElement viewport)
+        |> row [ spacing 20 ]
 
 
-thumbnailElement : String -> Element Msg
-thumbnailElement src =
+thumbnailElement : Viewport -> String -> Element Msg
+thumbnailElement viewport src =
+    let
+        sideLength =
+            (viewport.width - 700) // rowLength
+    in
     image
-        [ width (shrink |> maximum 200)
+        [ width (shrink |> maximum sideLength)
         , centerY
         , centerX
         , Border.solid
-        , Border.color debugColors.thumbnail
-        , Events.onClick (OpenImage src)
         ]
         { src = src, description = "" }
+        |> (\img -> link [ centerY ] { label = img, url = fullSizeUrl src })
         |> el
-            [ width (px 200)
-            , height (px 200)
+            [ width (px sideLength)
+            , height (px sideLength)
             , centerY
             , clip
-            , Background.color debugColors.el
             ]
 
 
-fullSizeElement : FullSizeData -> Element Msg
-fullSizeElement data =
+fullSizeElement : Viewport -> FullSizeData -> Element Msg
+fullSizeElement viewport data =
     let
         leftArrow =
             text "<"
-                |> el [ centerY, alignLeft ]
+                |> el [ centerY, centerX ]
                 |> el
                     [ Font.size (scaled 5)
                     , Events.onClick PrevImage
                     , alignLeft
                     , height fill
-                    , width (px 30)
-                    , Background.color debugColors.el
-                    , Background.color colors.lightGrey
+                    , width (px 50)
+                    , pointer
+                    , alpha 0.5
+                    , mouseOver [ alpha 0.7 ]
                     ]
 
         rightArrow =
             text ">"
-                |> el [ centerY, alignRight ]
+                |> el [ centerY, centerX ]
                 |> el
                     [ Font.size (scaled 5)
                     , Events.onClick NextImage
                     , alignRight
                     , height fill
-                    , width (px 30)
-                    , Background.color colors.lightGrey
+                    , width (px 50)
+                    , pointer
+                    , alpha 0.5
+                    , mouseOver [ alpha 0.7 ]
                     ]
     in
     row
@@ -290,24 +457,32 @@ fullSizeElement data =
         , width fill
         , height fill
         ]
-        [ leftArrow
-        , fullSizeImageElement data
-        , rightArrow
+        [ if not (SelectList.isHead data) then
+            leftArrow
+
+          else
+            none
+        , fullSizeImageElement viewport data
+        , if not (SelectList.isLast data) then
+            rightArrow
+
+          else
+            none
         ]
         |> el [ width fill, height fill ]
 
 
-fullSizeImageElement : FullSizeData -> Element Msg
-fullSizeImageElement data =
+fullSizeImageElement : Viewport -> FullSizeData -> Element Msg
+fullSizeImageElement viewport data =
     image
-        [ width (shrink |> maximum 1200)
-        , height (shrink |> maximum 550)
+        [ width (shrink |> maximum (viewport.width - 614))
+        , height (shrink |> maximum (viewport.height - 125))
         , centerX
-        , Events.onClick CloseImage
         ]
         { src = SelectList.selected data
         , description = ""
         }
+        |> (\img -> link [ centerX ] { label = img, url = thumbnailsUrl })
 
 
 siteHeader : Element Msg
@@ -344,7 +519,7 @@ nameElement name =
         , padding 10
         ]
         { label = text name
-        , url = "/"
+        , url = homeUrl
         }
 
 
@@ -368,6 +543,14 @@ scaled =
 
 
 
+-- CONSTANTS
+
+
+rowLength =
+    3
+
+
+
 -- PURE UTILITY
 
 
@@ -377,11 +560,7 @@ colors =
 
 
 debugColors =
-    { row = rgb 1 0 0
-    , el = rgb 0 1 0
-    , thumbnail = rgb 0 0 1
-    , column = rgb 0 0 0
-    }
+    {}
 
 
 chunksOf : Int -> List a -> List (List a)
